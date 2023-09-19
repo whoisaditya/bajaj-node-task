00251f60fead77d91fef5efd3680b432a33b2676
whoisaditya <mitraaditya1357@gmail.com>
2023-09-19 17:29:33 +0530

    fix: post alphabets

--- api/controllers/bfhl.js
+++ api/controllers/bfhl.js
@@ -9,39 +9,55 @@ exports.GetOpCode = async (req, res) => {
 
 exports.AddData = async (req, res) => {
   const { data } = req.body;
-  let maxAlphabet = "a";
   let numbers = [];
   let alphabets = [];
+  let highest_alphabet = [];
+
   let response = {
-    is_success: false,
+    is_success: true,
     user_id: process.env.USER_ID,
     email: process.env.COLLEGE_EMAIL,
     roll_number: process.env.COLLEGE_ROLL_NO,
     numbers: numbers,
     alphabets: alphabets,
-    highest_alphabet: [maxAlphabet],
+    highest_alphabet: highest_alphabet,
   };
 
   if (!data) {
+    response.is_success = false;
     return res.status(400).json(response);
   }
 
-  data.forEach((element) => {
-    const item = parseInt(element);
-    if (item) {
-      numbers.push(item);
-    } else {
-      maxAlphabet = Math.max(
-        maxAlphabet.toLowerCase().charCodeAt(0),
-        element.toLowerCase().charCodeAt(0)
-      );
-      maxAlphabet = String.fromCharCode(maxAlphabet);
-      alphabets.push(element);
+  data.forEach((item) => {
+    const ele = parseInt(item);
+    if (!isNaN(ele)) {
+      response.numbers.push(item.toString());
+    } else if (
+      typeof item === "string" &&
+      item.length === 1 &&
+      /^[a-zA-Z]+$/.test(item)
+    ) {
+      response.alphabets.push(item);
     }
   });
 
-  if(alphabets.length == 0) {
-    response.highest_alphabet = []
+  if (alphabets.length == 0) {
+    response.highest_alphabet = [];
+  } else {
+    alphabets.forEach((alphabet) => {
+      if (highest_alphabet.length == 0) {
+        highest_alphabet.push(alphabet);
+      } else {
+        let prev = highest_alphabet.pop();
+        // console.log(prev)
+        let curr = alphabet;
+        if(curr.toLowerCase().localeCompare(prev.toLowerCase())) {
+            highest_alphabet.push(curr)
+        } else {
+            highest_alphabet.push(prev)
+        }
+      }
+    });
   }
 
   return res.status(200).json(response);
